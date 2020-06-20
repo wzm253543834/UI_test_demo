a0a8d24e64416bde90306731e82ea18a7e23af8a
wzm253543834 <253543834@qq.com>
2020-06-20 18:01:14 +0800

    UI test demo

--- pageobjects/lemobar_login.py
+++ pageobjects/lemobar_login.py
@@ -9,18 +9,23 @@ class HomePage(BasePage):
     index_code = "xpath=>//*[@id='app']/section/div[2]/div/form/div[5]/div[1]/div/div/input"
     index_enter = "xpath=>//*[@id='app']/section/div[2]/div/form/div[7]/div/div/button"
 
+    # 清空验证码窗口
     def clear_code(self):
         self.clear(self.index_code)
 
+    # 输入账号
     def input_username(self, text):
         self.type(self.index_username, text)
 
+    # 输入密码
     def input_password(self, text):
         self.type(self.index_password, text)
 
+    # 按登录按钮
     def click_login_button(self):
         self.click(self.index_enter)
 
+    # 登录
     def login(self):
         self.input_username('wangzm')
         self.input_password('a645765783')
