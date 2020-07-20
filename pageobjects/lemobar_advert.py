92c95710484cf95661d2a8474884fea1bcae7a4a
wzm253543834 <253543834@qq.com>
2020-07-20 17:28:07 +0800

    UI test demo

--- pageobjects/lemobar_advert.py
+++ pageobjects/lemobar_advert.py
@@ -57,3 +57,313 @@ class AdvertPage(BasePage):
     def find_tab_countdown(self):
         a = self.get_tab_title(self.tab_countDown)
         return a
+
+
+class AdvertSearch(BasePage):
+
+    advert_advertid_search = "xpath=>//*[@id='search_list']/div[1]/div/div/input"
+    advert_adverttitle_search = "xpath=>//*[@id='search_list']/div[2]/div/div/input"
+    advert_isenable_search = "xpath=>//*[@id='search_list']/div[3]/div/div/div[1]/input"
+    advert_search_button = "xpath=>//*[@id='search_list']/div[4]/div/button[1]"
+    advert_click_button = "xpath=>//*[@id='search_list']/div[4]/div/button[2]"
+    advert_isenable = "xpath=>/html/body/div[2]/div[1]/div[1]/ul/li[1]"
+
+    """
+    广告管理各筛选项
+    """
+
+    # 广告ID查询
+    def advertid_search(self, text):
+        self.type(self.advert_advertid_search, text)
+
+    # 广告标题查询
+    def adverttitle_search(self, text):
+        self.type(self.advert_adverttitle_search, text)
+
+    # 是否启用查询
+    def isenable_search(self):
+        self.click(self.advert_isenable_search)
+        self.sleep(2)
+        self.click(self.advert_isenable)
+
+    # 查询按钮
+    def search_button_click(self):
+        self.click(self.advert_search_button)
+
+    # 清空按钮
+    def click_button_click(self):
+        self.click(self.advert_click_button)
+
+    # 测试结果断言
+    result_advertid = "xpath=>//*[@id='app']/div/div/div[2]/div/div[2]/section/div[1]/div[3]/table/tbody/tr/td[2]/div"
+    result_adverttitle = "xpath=>//*[@id='app']/div/div/div[2]/div/div[2]/section/div[1]/div[3]/table/tbody/tr/td[3]/div"
+    result_isenable = "xpath=>//*[@id='app']/div/div/div[2]/div/div[2]/section/div[1]/div[3]/table/tbody/tr/td[6]/div/i"
+
+    # 断言广告ID查询结果
+    def find_result_advertid(self):
+        a = self.get_search_result(self.result_advertid)
+        return a
+
+    # 断言广告标题查询结果
+    def find_result_adverttitle(self):
+        a = self.get_search_result(self.result_adverttitle)
+        return a
+
+    # 断言是否启用查询结果
+    def find_result_isenable(self):
+        a = self.get_search_result_icon(self.result_isenable)
+        return a
+
+
+class AdvertScheduleSearch(BasePage):
+
+    advertSchedule_begintime_search = "xpath=>//*[@id='search_list']/div[1]/div/div/input"
+    advertSchedule_endtime_search = "xpath=>//*[@id='search_list']/div[2]/div/div/input"
+    advertSchedule_areaid_search = "xpath=>//*[@id='search_list']/div[3]/div/div/input"
+    advertSchedule_areaname_search = "xpath=>//*[@id='search_list']/div[4]/div/div/input"
+    advertSchedule_isenable_search = "xpath=>//*[@id='search_list']/div[5]/div/div/div[1]/input"
+    advertSchedule_effecttype_search = "xpath=>//*[@id='search_list']/div[7]/div/div/div[1]/input"
+    advertSchedule_effectchannel_search = "xpath=>//*[@id='search_list']/div[8]/div/div/div/input"
+    advertSchedule_search_button = "xpath=>//*[@id='search_list']/div[6]/div/button"
+    advertSchedule_click_button = "xpath=>//*[@id='search_list']/div[9]/div/button"
+    advertSchedule_begintime = "xpath=>/html/body/div[3]/div[1]/div/div[2]/table[1]/tbody/tr[3]/td[3]/div"
+    advertSchedule_endtime = "xpath=>/html/body/div[3]/div[1]/div/div[2]/table[1]/tbody/tr[3]/td[2]/div"
+    advertSchedule_isenable = "xpath=>/html/body/div[4]/div[1]/div[1]/ul/li[1]"
+    advertSchedule_effecttype = "xpath=>/html/body/div[5]/div[1]/div[1]/ul/li[1]"
+    advertSchedule_effectchannel = "xpath=>/html/body/div[6]/div[1]/div[1]/ul/li[1]"
+
+    """
+    广告排期各筛选项
+    """
+
+    # 开始日期查询
+    def begintime_search(self):
+        self.click(self.advertSchedule_begintime_search)
+        self.sleep(2)
+        self.click(self.advertSchedule_begintime)
+
+    # 结束日期查询
+    def endtime_search(self):
+        self.click(self.advertSchedule_endtime_search)
+        self.sleep(2)
+        self.click(self.advertSchedule_endtime)
+
+    # 网点ID查询
+    def areaid_search(self, text):
+        self.type(self.advertSchedule_areaid_search, text)
+
+    # 网点名称查询
+    def areaname_search(self, text):
+        self.type(self.advertSchedule_areaname_search, text)
+
+    # 是否可用查询
+    def isenable_search(self):
+        self.click(self.advertSchedule_isenable_search)
+        self.sleep(2)
+        self.click(self.advertSchedule_isenable)
+
+    # 作用类型查询
+    def effecttype_search(self):
+        self.click(self.advertSchedule_effecttype_search)
+        self.sleep(2)
+        self.click(self.advertSchedule_effecttype)
+
+    # 作用渠道查询
+    def effectchannel_search(self):
+        self.click(self.advertSchedule_effectchannel_search)
+        self.sleep(2)
+        self.click(self.advertSchedule_effectchannel)
+
+    # 查询按钮
+    def search_button_click(self):
+        self.click(self.advertSchedule_search_button)
+
+    # 清空按钮
+    def click_button_click(self):
+        self.click(self.advertSchedule_click_button)
+
+    # 测试结果断言
+    result_begintime = "xpath=>//*[@id='app']/div/div/div[2]/div/div[2]/section/div[2]/div/div[3]/table/tbody/tr/td[6]/div"
+    result_endtime = "xpath=>//*[@id='app']/div/div/div[2]/div/div[2]/section/div[2]/div/div[3]/table/tbody/tr/td[7]/div"
+    result_isenable= "xpath=>//*[@id='app']/div/div/div[2]/div/div[2]/section/div[2]/div/div[3]/table/tbody/tr/td[9]/div/i"
+    result_effecttype = "xpath=>//*[@id='app']/div/div/div[2]/div/div[2]/section/div[2]/div/div[3]/table/tbody/tr/td[4]/div"
+    result_effectchannel = "xpath=>//*[@id='app']/div/div/div[2]/div/div[2]/section/div[2]/div/div[3]/table/tbody/tr/td[5]/div"
+
+    # 断言开始日期查询结果
+    def find_result_begintime(self):
+        a = self.get_search_result(self.result_begintime)
+        return a
+
+    # 断言结束日期查询结果
+    def find_result_endtime(self):
+        a = self.get_search_result(self.result_endtime)
+        return a
+
+    # 断言是否可用查询结果
+    def find_result_isenable(self):
+        a = self.get_search_result_icon(self.result_isenable)
+        return a
+
+    # 断言作用类型查询结果
+    def find_result_effecttype(self):
+        a = self.get_search_result(self.result_effecttype)
+        return a
+
+    # 断言作用渠道查询结果
+    def find_result_effectchannel(self):
+        a = self.get_search_result(self.result_effectchannel)
+        return a
+
+
+class BrandMapSearch(BasePage):
+
+    brandMap_brandMapid_search = "xpath=>//*[@id='search_list']/div[1]/div/div/input"
+    brandMap_brandMaptitle_search = "xpath=>//*[@id='search_list']/div[2]/div/div/input"
+    brandMap_isenable_search = "xpath=>//*[@id='search_list']/div[3]/div/div/div[1]/input"
+    brandMap_effectchannel_search = "xpath=>//*[@id='search_list']/div[4]/div/div/div[1]/input"
+    brandMap_search_button = "xpath=>//*[@id='search_list']/div[5]/div/button[1]"
+    brandMap_click_button = "xpath=>//*[@id='search_list']/div[5]/div/button[2]"
+    brandMap_isenable = "xpath=>/html/body/div[4]/div[1]/div[1]/ul/li[1]"
+    brandMap_effectchannel = "xpath=>/html/body/div[5]/div[1]/div[1]/ul/li[1]"
+
+    """
+    支付页背景图各筛选项
+    """
+
+    # 背景图ID查询
+    def brandmapid_search(self, text):
+        self.type(self.brandMap_brandMapid_search, text)
+
+    # 背景图标题查询
+    def brandmaptitle_search(self, text):
+        self.type(self.brandMap_brandMaptitle_search, text)
+
+    # 是否启用查询
+    def isenable_search(self):
+        self.click(self.brandMap_isenable_search)
+        self.sleep(2)
+        self.click(self.brandMap_isenable)
+
+    # 作用渠道查询
+    def effectchannel_search(self):
+        self.click(self.brandMap_effectchannel_search)
+        self.sleep(2)
+        self.click(self.brandMap_effectchannel)
+
+    # 查询按钮
+    def search_button_click(self):
+        self.click(self.brandMap_search_button)
+
+    # 清空按钮
+    def click_button_click(self):
+        self.click(self.brandMap_click_button)
+
+    # 测试结果断言
+    result_brandMapid = "xpath=>//*[@id='app']/div/div/div[2]/div/div[2]/section/div[1]/div[3]/table/tbody/tr/td[2]/div"
+    result_brandMaptitle = "xpath=>//*[@id='app']/div/div/div[2]/div/div[2]/section/div[1]/div[3]/table/tbody/tr/td[3]/div"
+    result_isenable = "xpath=>//*[@id='app']/div/div/div[2]/div/div[2]/section/div[1]/div[3]/table/tbody/tr/td[7]/div/i"
+    result_effectchannel = "xpath=>//*[@id='app']/div/div/div[2]/div/div[2]/section/div[1]/div[3]/table/tbody/tr/td[6]/div"
+
+    # 断言背景图ID查询结果
+    def find_result_brandmapid(self):
+        a = self.get_search_result(self.result_brandMapid)
+        return a
+
+    # 断言背景图标题查询结果
+    def find_result_brandmaptitle(self):
+        a = self.get_search_result(self.result_brandMaptitle)
+        return a
+
+    # 断言是否启用查询结果
+    def find_result_isenable(self):
+        a = self.get_search_result(self.result_isenable)
+        return a
+
+    # 断言作用渠道查询结果
+    def find_result_effectchannel(self):
+        a = self.get_search_result(self.result_effectchannel)
+        return a
+
+
+class CountDownSearch(BasePage):
+
+    countDown_popupid_search = "xpath=>//*[@id='search_list']/div[1]/div/div/input"
+    countDown_popupname_search = "xpath=>//*[@id='search_list']/div[2]/div/div/input"
+    countDown_effectivestatus_search = "xpath=>//*[@id='search_list']/div[3]/div/div/div[2]/input"
+    countDown_effectchannel_search = "xpath=>//*[@id='search_list']/div[4]/div/div/div[1]/input"
+    countDown_popuptype_search = "xpath=>//*[@id='search_list']/div[5]/div/div/div[1]/input"
+    countDown_search_button = "xpath=>//*[@id='search_list']/div[6]/div/button[1]"
+    countDown_click_button = "xpath=>//*[@id='search_list']/div[6]/div/button[2]"
+    countDown_effectivestatus = "xpath=>/html/body/div[4]/div[1]/div[1]/ul/li[1]"
+    countDown_effectchannel = "xpath=>/html/body/div[5]/div[1]/div[1]/ul/li[1]"
+    countDown_popuptype = "xpath=>/html/body/div[6]/div[1]/div[1]/ul/li[1]"
+
+    """
+    倒计时页弹窗各筛选项
+    """
+
+    # 弹窗ID查询
+    def popupid_search(self, text):
+        self.type(self.countDown_popupid_search, text)
+
+    # 弹窗名称查询
+    def popupname_search(self, text):
+        self.type(self.countDown_popupname_search, text)
+
+    # 生效状态查询
+    def effectivestatus_search(self):
+        self.click(self.countDown_effectivestatus_search)
+        self.sleep(2)
+        self.click(self.countDown_effectivestatus)
+
+    # 作用渠道查询
+    def effectchannel_search(self):
+        self.click(self.countDown_effectchannel_search)
+        self.sleep(2)
+        self.click(self.countDown_effectchannel)
+
+    # 弹窗类型查询
+    def popuptype_search(self):
+        self.click(self.countDown_popuptype_search)
+        self.sleep(2)
+        self.click(self.countDown_popuptype)
+
+    # 查询按钮
+    def search_button_click(self):
+        self.click(self.countDown_search_button)
+
+    # 清空按钮
+    def click_button_click(self):
+        self.click(self.countDown_click_button)
+
+    # 测试结果断言
+    result_popupid = "xpath=>//*[@id='countDown']/div[1]/div[3]/table/tbody/tr/td[2]/div"
+    result_popupname = "xpath=>//*[@id='countDown']/div[1]/div[3]/table/tbody/tr/td[3]/div"
+    result_effectivestatus = "xpath=>//*[@id='countDown']/div[1]/div[3]/table/tbody/tr[1]/td[9]/div/span"
+    result_effectchannel = "xpath=>//*[@id='countDown']/div[1]/div[3]/table/tbody/tr[1]/td[6]/div/span"
+    result_popuptype = "xpath=>//*[@id='countDown']/div[1]/div[3]/table/tbody/tr[1]/td[4]/div/span"
+
+    # 断言弹窗ID查询结果
+    def find_result_popupid(self):
+        a = self.get_search_result(self.result_popupid)
+        return a
+
+    # 断言弹窗名称查询结果
+    def find_result_popupname(self):
+        a = self.get_search_result(self.result_popupname)
+        return a
+
+    # 断言生效状态查询结果
+    def find_result_effectivestatus(self):
+        a = self.get_search_result(self.result_effectivestatus)
+        return a
+
+    # 断言作用渠道查询结果
+    def find_result_effectchannel(self):
+        a = self.get_search_result(self.result_effectchannel)
+        return a
+
+    # 断言弹窗类型查询结果
+    def find_result_popuptype(self):
+        a = self.get_search_result(self.result_popuptype)
+        return a
--- testsuites/test_lemobar_advert.py
+++ testsuites/test_lemobar_advert.py
@@ -3,6 +3,10 @@ import unittest
 from framework.browser_engine import BrowserEngine
 from pageobjects.lemobar_login import HomePage
 from pageobjects.lemobar_advert import AdvertPage
+from pageobjects.lemobar_advert import AdvertSearch
+from pageobjects.lemobar_advert import AdvertScheduleSearch
+from pageobjects.lemobar_advert import BrandMapSearch
+from pageobjects.lemobar_advert import CountDownSearch
 
 
 class AdvertPageSearch(unittest.TestCase):
@@ -58,6 +62,277 @@ class AdvertPageSearch(unittest.TestCase):
             print('Test Pass')
         except Exception as e:
             print('Test Fail', format(e))
+
+        newsearch = AdvertSearch(self.driver)
+
+        # 广告ID查询
+        newsearch.advertid_search('1')
+        newsearch.search_button_click()
+        newsearch.sleep(2)
+        try:
+            assert '1' in newsearch.find_result_advertid()
+            print('Test Pass')
+        except Exception as e:
+            print('Test Fail', format(e))
+        newsearch.click_button_click()
+        newsearch.sleep(2)
+
+        # 广告标题查询
+        newsearch.adverttitle_search('眼部按摩仪')
+        newsearch.search_button_click()
+        newsearch.sleep(2)
+        try:
+            assert '眼部按摩仪' in newsearch.find_result_adverttitle()
+            print('Test Pass')
+        except Exception as e:
+            print('Test Fail', format(e))
+        newsearch.click_button_click()
+        newsearch.sleep(2)
+
+        # 是否启用查询
+        newsearch.isenable_search()
+        newsearch.search_button_click()
+        newsearch.sleep(2)
+        try:
+            assert 'el-icon-circle-check' in newsearch.find_result_isenable()
+            print('Test Pass')
+        except Exception as e:
+            print('Test Fail', format(e))
+        newsearch.click_button_click()
+        newsearch.sleep(2)
+
+    def test_c_advertschedule_search(self):
+        """
+        广告排期查询
+        :return:
+        """
+        homepage = AdvertPage(self.driver)
+        homepage.search_click_advert_advertschedule()
+        homepage.sleep(3)
+        homepage.get_window_img()
+        try:
+            assert '广告排期' in homepage.find_tab_advertschedule()
+            print('Test Pass')
+        except Exception as e:
+            print('Test Fail', format(e))
+
+        newsearch = AdvertScheduleSearch(self.driver)
+
+        # 开始日期查询
+        newsearch.begintime_search()
+        newsearch.search_button_click()
+        newsearch.sleep(2)
+        try:
+            assert '2020-07-01' in newsearch.find_result_begintime()
+            print('Test Pass')
+        except Exception as e:
+            print('Test Fail', format(e))
+        newsearch.click_button_click()
+        newsearch.sleep(2)
+
+        # 结束日期查询
+        newsearch.endtime_search()
+        newsearch.search_button_click()
+        newsearch.sleep(2)
+        try:
+            assert '2020-07-31' in newsearch.find_result_endtime()
+            print('Test Pass')
+        except Exception as e:
+            print('Test Fail', format(e))
+        newsearch.click_button_click()
+        newsearch.sleep(2)
+
+        # 网点ID查询
+        newsearch.areaid_search('22515')
+        newsearch.search_button_click()
+        newsearch.sleep(2)
+        newsearch.click_button_click()
+        newsearch.sleep(2)
+
+        # 网点名称查询
+        newsearch.areaname_search('福州')
+        newsearch.search_button_click()
+        newsearch.sleep(2)
+        newsearch.click_button_click()
+        newsearch.sleep(2)
+
+        # 是否可用查询
+        newsearch.isenable_search()
+        newsearch.search_button_click()
+        newsearch.sleep(2)
+        try:
+            assert 'el-icon-circle-check' in newsearch.find_result_isenable()
+            print('Test Pass')
+        except Exception as e:
+            print('Test Fail', format(e))
+        newsearch.click_button_click()
+        newsearch.sleep(2)
+
+        # 作用类型查询
+        newsearch.effecttype_search()
+        newsearch.search_button_click()
+        newsearch.sleep(2)
+        try:
+            assert '支付页' in newsearch.find_result_effecttype()
+            print('Test Pass')
+        except Exception as e:
+            print('Test Fail', format(e))
+        newsearch.click_button_click()
+        newsearch.sleep(2)
+
+        # 作用渠道查询
+        newsearch.effectchannel_search()
+        newsearch.search_button_click()
+        newsearch.sleep(2)
+        try:
+            assert '微信H5' in newsearch.find_result_effectchannel()
+            print('Test Pass')
+        except Exception as e:
+            print('Test Fail', format(e))
+        newsearch.click_button_click()
+        newsearch.sleep(2)
+
+    def test_d_brandmap_search(self):
+        """
+        支付页背景图查询
+        :return:
+        """
+        homepage = AdvertPage(self.driver)
+        homepage.search_click_advert_brandmap()
+        homepage.sleep(3)
+        homepage.get_window_img()
+        try:
+            assert '支付页背景图' in homepage.find_tab_brandmap()
+            print('Test Pass')
+        except Exception as e:
+            print('Test Fail', format(e))
+
+        newsearch = BrandMapSearch(self.driver)
+
+        # 背景图ID查询
+        newsearch.brandmapid_search('7')
+        newsearch.search_button_click()
+        newsearch.sleep(2)
+        try:
+            assert '7' in newsearch.find_result_brandmapid()
+            print('Test Pass')
+        except Exception as e:
+            print('Test Fail', format(e))
+        newsearch.click_button_click()
+        newsearch.sleep(2)
+
+        # 背景图标题查询
+        newsearch.brandmaptitle_search('湖北')
+        newsearch.search_button_click()
+        newsearch.sleep(2)
+        try:
+            assert '湖北' in newsearch.find_result_brandmaptitle()
+            print('Test Pass')
+        except Exception as e:
+            print('Test Fail', format(e))
+        newsearch.click_button_click()
+        newsearch.sleep(2)
+
+        # 是否启用查询
+        newsearch.isenable_search()
+        newsearch.search_button_click()
+        newsearch.sleep(2)
+        try:
+            assert 'el-icon-circle-check' in newsearch.find_result_isenable()
+            print('Test Pass')
+        except Exception as e:
+            print('Test Fail', format(e))
+        newsearch.click_button_click()
+        newsearch.sleep(2)
+
+        # 作用渠道查询
+        newsearch.effectchannel_search()
+        newsearch.search_button_click()
+        newsearch.sleep(2)
+        try:
+            assert '微信H5' in newsearch.find_result_effectchannel()
+            print('Test Pass')
+        except Exception as e:
+            print('Test Fail', format(e))
+        newsearch.click_button_click()
+        newsearch.sleep(2)
+
+    def test_e_countdown_search(self):
+        """
+        倒计时页弹窗查询
+        :return:
+        """
+        homepage = AdvertPage(self.driver)
+        homepage.search_click_advert_countdown()
+        homepage.sleep(3)
+        homepage.get_window_img()
+        try:
+            assert '支付页背景图' in homepage.find_tab_countdown()
+            print('Test Pass')
+        except Exception as e:
+            print('Test Fail', format(e))
+
+        newsearch = CountDownSearch(self.driver)
+
+        # 弹窗ID查询
+        newsearch.popupid_search('120')
+        newsearch.search_button_click()
+        newsearch.sleep(2)
+        try:
+            assert '120' in newsearch.find_result_popupid()
+            print('Test Pass')
+        except Exception as e:
+            print('Test Fail', format(e))
+        newsearch.click_button_click()
+        newsearch.sleep(2)
+
+        # 弹窗名称查询
+        newsearch.popupname_search('测试')
+        newsearch.search_button_click()
+        newsearch.sleep(2)
+        try:
+            assert '测试' in newsearch.find_result_popupname()
+            print('Test Pass')
+        except Exception as e:
+            print('Test Fail', format(e))
+        newsearch.click_button_click()
+        newsearch.sleep(2)
+
+        # 生效状态查询
+        newsearch.effectivestatus_search()
+        newsearch.search_button_click()
+        newsearch.sleep(2)
+        try:
+            assert '生效中' in newsearch.find_result_effectivestatus()
+            print('Test Pass')
+        except Exception as e:
+            print('Test Fail', format(e))
+        newsearch.click_button_click()
+        newsearch.sleep(2)
+
+        # 作用渠道查询
+        newsearch.effectchannel_search()
+        newsearch.search_button_click()
+        newsearch.sleep(2)
+        try:
+            assert '微信H5' or '不限' in newsearch.find_result_effectchannel()
+            print('Test Pass')
+        except Exception as e:
+            print('Test Fail', format(e))
+        newsearch.click_button_click()
+        newsearch.sleep(2)
+
+        # 弹窗类型查询
+        newsearch.popuptype_search()
+        newsearch.search_button_click()
+        newsearch.sleep(2)
+        try:
+            assert '活动弹窗' in newsearch.find_result_popuptype()
+            print('Test Pass')
+        except Exception as e:
+            print('Test Fail', format(e))
+        newsearch.click_button_click()
+        newsearch.sleep(2)
 
 
 if __name__ == '__main__':
